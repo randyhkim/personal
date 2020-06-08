499bcfe86e81f4fd300cb477c4662d525cde88d7
randyhkim <hgrkim@gmail.com>
2020-06-08 15:06:18 +0900

    Edit Structures and Show Player Specs
    Edited structures and added values to warrior stats. The program now prints the warrior's
    stats. Also, there is a minor addition of a function named 'accept' that prompts an accept
    button to the user.

--- struct.c
+++ struct.c
@@ -5,13 +5,7 @@
 
 #define NAME_MAX 21
 #define LEVELCAP 4
-
-struct player_stats {
-    unsigned int HP;
-    unsigned int MP;
-    unsigned int XP;
-    unsigned int LVL;
-};
+#define PREMADE_WARRIORS 5
 
 struct player_specs {
     unsigned int STR : 4;
@@ -21,10 +15,21 @@ struct player_specs {
     unsigned int INT : 4;
     unsigned int AGL : 4;
     unsigned int LCK : 4;
+} specs;
+
+struct player_stats {
+    char name[10];
+    unsigned int HP;
+    unsigned int MP;
+    unsigned int XP;
+    unsigned int LVL;
+    struct player_specs specs;
 };
 
 char *enter_name(void);
 int character_init(char *str);
+void character_stat(struct player_stats warrior);
+void accept(void);
 
 
 void main(void)
@@ -32,9 +37,83 @@ void main(void)
     char *player_name;
     int choice;
 
+    struct player_stats warriors[PREMADE_WARRIORS];
+    struct player_specs specs;
+
+    strcpy(warriors[0].name, "Hogang");
+    warriors[0].HP = 100;
+    warriors[0].MP = 100;
+    warriors[0].XP = 0;
+    warriors[0].LVL = 1;
+    warriors[0].specs.PER = 3;
+    warriors[0].specs.STR = 3;
+    warriors[0].specs.END = 3;
+    warriors[0].specs.CHR = 3;
+    warriors[0].specs.INT = 3;
+    warriors[0].specs.AGL = 3;
+    warriors[0].specs.LCK = 3;
+
+    strcpy(warriors[1].name, "Woon");
+    warriors[1].HP = 100;
+    warriors[1].MP = 100;
+    warriors[1].XP = 0;
+    warriors[1].LVL = 1;
+    warriors[1].specs.PER = 3;
+    warriors[1].specs.STR = 3;
+    warriors[1].specs.END = 3;
+    warriors[1].specs.CHR = 3;
+    warriors[1].specs.INT = 3;
+    warriors[1].specs.AGL = 3;
+    warriors[1].specs.LCK = 3;
+
+    strcpy(warriors[2].name, "Bug");
+    warriors[2].HP = 100;
+    warriors[2].MP = 100;
+    warriors[2].XP = 0;
+    warriors[2].LVL = 1;
+    warriors[2].specs.PER = 3;
+    warriors[2].specs.STR = 3;
+    warriors[2].specs.END = 3;
+    warriors[2].specs.CHR = 3;
+    warriors[2].specs.INT = 3;
+    warriors[2].specs.AGL = 3;
+    warriors[2].specs.LCK = 3;
+
+    strcpy(warriors[3].name, "Juice");
+    warriors[3].HP = 100;
+    warriors[3].MP = 100;
+    warriors[3].XP = 0;
+    warriors[3].LVL = 1;
+    warriors[3].specs.PER = 3;
+    warriors[3].specs.STR = 3;
+    warriors[3].specs.END = 3;
+    warriors[3].specs.CHR = 3;
+    warriors[3].specs.INT = 3;
+    warriors[3].specs.AGL = 3;
+    warriors[3].specs.LCK = 3;
+
+    strcpy(warriors[4].name, "Diva");
+    warriors[4].HP = 100;
+    warriors[4].MP = 100;
+    warriors[4].XP = 0;
+    warriors[4].LVL = 1;
+    warriors[4].specs.PER = 3;
+    warriors[4].specs.STR = 3;
+    warriors[4].specs.END = 3;
+    warriors[4].specs.CHR = 3;
+    warriors[4].specs.INT = 3;
+    warriors[4].specs.AGL = 3;
+    warriors[4].specs.LCK = 3;
+
+    printf("STR is %d\n", warriors[0].specs.STR);
+
     player_name = enter_name();
 
     choice = character_init(player_name);
+
+    character_stat(warriors[choice - 1]);
+    
+    accept();
 }
 
 
@@ -59,7 +138,7 @@ char *enter_name(void)
         }
     }
 
-    printf("\nWelcome, %s\n\n", str);
+    printf("\n[Welcome, %s]\n\n", str);
     return str;
 }
 
@@ -77,6 +156,7 @@ int character_init(char *str)
 
     printf("\t1. American Hogang\n");
     printf("\t\t패시브: Bald Eagle\n");
+    printf("\t\t설명: 전직 시 어학병 선택 가능.\n");
     printf("\n");
     printf("\t2. Rap-monster Woon\n");
     printf("\t\t패시브: Hong-dae Master\n");
@@ -87,17 +167,54 @@ int character_init(char *str)
     printf("\t4. Sleeping Juice\n");
     printf("\t\t패시브: Natural Leader\n");
     printf("\n");
+    printf("\t5. Diva\n");
+    printf("\t\t패시브: Busan Gashina\n");
+    printf("\n");
 
     do {
-        printf("번호를 입력하여 캐릭터를 선택하십시오.\n");
-        printf("기억하십시오 %s님. 당신이 고른 캐릭터로 군생활을 하는것입니다.\n", str);
+        printf("번호를 입력하여 전사를 선택하십시오.\n");
+        printf("기억하십시오 %s님. 당신이 고른 전사로 군생활을 하는것입니다.\n", str);
         fgets(a, sizeof(a), stdin);
         /* TODO: look into the meaning of sizeof(a) here. Also, if I use 3
                  instead of sizeof(a), the do-while loop seems to run
                  automatically when inputting multi-character strings. */
         b = atoi(a);
-    } while (b > 4 || b < 1);
+    } while (b > PREMADE_WARRIORS || b < 1);
 
-    printf("You have chosen %d\n", b);
+    printf("\n[You have chosen %d]\n\n", b);
     return b;
+}
+
+/* Read and edit character stats. */
+void character_stat(struct player_stats warrior)
+{
+    printf("자세히 보니, 내 이름은 %s이군...\n", warrior.name);
+    printf("[검진표를 자세히 보니 수치가 보인다]\n\n");
+    printf("=====================================\n");
+    printf("==== HP ==== MP ==== XP ==== LVL ====\n");
+    printf("=====================================\n");
+    printf("|  %4d    %4d    %4d      %4d   |\n", warrior.HP, warrior.MP, warrior.XP, warrior.LVL);
+    printf("=====================================\n");
+    printf("=====================================\n");
+    printf("|   STR || %d                        |\n", warrior.specs.STR);
+    printf("|   PER || %d                        |\n", warrior.specs.PER);
+    printf("|   END || %d                        |\n", warrior.specs.END);
+    printf("|   CHR || %d                        |\n", warrior.specs.CHR);
+    printf("|   INT || %d                        |\n", warrior.specs.INT);
+    printf("|   AGL || %d                        |\n", warrior.specs.AGL);
+    printf("|   LCK || %d                        |\n", warrior.specs.LCK);
+    printf("=====================================\n");
+    printf("\n");
+    // TODO: Add functionality to edit character stats.
+}
+
+/* Prompt 확인 button */
+void accept(void)
+{
+    char s[256];
+    do {
+        printf("[Enter눌러서 확인]\n");
+        fgets(s, sizeof(s), stdin);
+    } while (s[0] != '\n');
+    printf("확인완료\n");
 }
